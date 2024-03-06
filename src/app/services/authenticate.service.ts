405e28aec6d9862d555d4d2ceac05e83e4c2630e
Arthur Gibert <arthurgibert@free.fr>
2024-03-06 13:33:03 +0100

    Ajout d'EncryptDecrypt Service

--- src/app/services/authenticate.service.ts
+++ src/app/services/authenticate.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { User } from '../model/user.model';
 import { ApiService } from './api.service';
+import { EncryptDecryptService } from './encrypt-decrypt.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class AuthenticateService {
   listUser: User[] = [];
   error : string | undefined | null;
 
-  constructor(private apiService : ApiService) {
+  constructor(private apiService : ApiService, private encryptService : EncryptDecryptService) {
     this.getAllUsers();
   }
 
@@ -32,10 +33,36 @@ export class AuthenticateService {
     const existingUser = this.listUser.find(user => user.password === userToCheck.password && user.email === userToCheck.email)
     if(existingUser) {
       console.log("L'utilisateur est connecté")
+      this.saveUserInLocalStorage(existingUser);
+      this.getUserfromLocalStorage();
     }else {
       console.log("L'utilisateur n'existe pas !")
     }
   }
+
+  /**
+   * Enregistre le client dans le LocalStorage tout en cryptant ces données
+   */
+    saveUserInLocalStorage(user : User) {
+      user.password = this.encryptService.encrypt(user.password);
+      user.email = this.encryptService.encrypt(user.email);
+      localStorage.setItem('user', JSON.stringify(user));
+    }
+    
+  /**
+   * Récupère le client depuis le LocalStorage tout en décryptant ces données
+   */
+    getUserfromLocalStorage() {
+      const encryptUserData = localStorage.getItem('user');
+      if(encryptUserData) {
+        const user = JSON.parse(encryptUserData);
+        user.password = this.encryptService.decrypt(user.password);
+        user.email = this.encryptService.decrypt(user.email);
+        console.log(user);
+      }else {
+        console.log("NADA")
+      }
+    }
 
   isAdmin() {
     return true;
