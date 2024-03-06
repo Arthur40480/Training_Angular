8c575072b1f1505f577ac426d7b7d8ac5ca10533
Arthur Gibert <arthurgibert@free.fr>
2024-03-06 22:17:48 +0100

    Admin composant html

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Training } from 'src/app/model/training.model';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
+  listTrainings : Training[] | undefined;
+  error : string | undefined | null;
 
-  constructor() { }
+  constructor(private router : Router, private apiService : ApiService) { }
 
   ngOnInit(): void {
+    this.getAllTrainings();
   }
 
+  getAllTrainings() {
+    this.apiService.getTrainings().subscribe({
+      next : (data) => this.listTrainings = data,
+      error : (err) => this.error = err.message,
+      complete : () => this.error = null
+    })
+  }
 }
